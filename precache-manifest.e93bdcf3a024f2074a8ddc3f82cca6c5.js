afae4f716187b64e48d395726202fa1e07164d09
Patricia de Menezes <paty7sp@gmail.com>
2020-05-06 08:46:06 -0300

    Updates

---
+++ precache-manifest.e93bdcf3a024f2074a8ddc3f82cca6c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6629b2aa45fb5cc71e79d29754291c52",
+    "url": "/Contador/index.html"
+  },
+  {
+    "revision": "a2559905e1f6667bf560",
+    "url": "/Contador/static/css/main.25692e48.chunk.css"
+  },
+  {
+    "revision": "db84acf5aeb05ee5b86d",
+    "url": "/Contador/static/js/2.bdb451af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Contador/static/js/2.bdb451af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2559905e1f6667bf560",
+    "url": "/Contador/static/js/main.8a80cfbb.chunk.js"
+  },
+  {
+    "revision": "bb77349ceef4432bd086",
+    "url": "/Contador/static/js/runtime-main.0dc902be.js"
+  },
+  {
+    "revision": "06d881abfa436406c197e2a8f37ad986",
+    "url": "/Contador/static/media/logo.06d881ab.svg"
+  }
+]);
